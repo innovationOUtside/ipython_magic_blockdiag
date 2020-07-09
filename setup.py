d98e337dab2dc111401bad1b99bb966779971e3d
Tony Hirst <tony.hirst@gmail.com>
2020-07-09 09:30:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(name='blockdiagMagic',
       packages=['blockdiag_magic'],
       install_requires=['blockdiag']
-#also support a ipython_magic_blockdiag[svg] opton that installs wand as well
+#also support a ipython_magic_blockdiag[svg] opton that installs Wand as well
      )
