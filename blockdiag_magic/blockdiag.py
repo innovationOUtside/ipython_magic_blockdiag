5a71f4d6f73cec3e13775675bd3849ccc265f23c
Tony Hirst <tony.hirst@gmail.com>
2020-07-09 10:45:33 +0100

    Update blockdiag.py

--- blockdiag_magic/blockdiag.py
+++ blockdiag_magic/blockdiag.py
@@ -1,12 +1,4 @@
 #via https://gist.github.com/sherdim/f0293bc7b022b852185b
-
-
-# -*- coding: utf-8 -*-
-from __future__ import print_function
-from IPython.core.magic import (Magics, magics_class, line_magic,
-                                cell_magic, line_cell_magic)
-                                
-# -*- coding: utf-8 -*-
 """magics for using blockdiag.com modules with IPython Notebook
 
 The module provides magics:  %%actdiag, %%blockdiag, %%nwdiag, %%seqdiag
@@ -54,19 +46,19 @@ from IPython.core.magic import Magics, magics_class, cell_magic, line_cell_magic
 from IPython.core.displaypub import publish_display_data
 from IPython.display import SVG, display
 
-_draw_mode = 'SVG'
-_publish_mode = 'SVG'
-
-_inkscape_available = False
-
 @magics_class
 class BlockdiagMagics(Magics):
     """Magics for blockdiag and others"""
-
-    def _import_all(self, module):
-        for k, v in module.__dict__.items():
-            if not k.startswith('__'):
-                self.shell.push({k:v})
+    
+    def __init__(self, shell):
+        super(BlockdiagMagics, self).__init__(shell)
+        self._publish_mode='SVG'
+        self._publish_mode = self._publish_mode.lower()
+    
+    #def _import_all(self, module):
+    #    for k, v in module.__dict__.items():
+    #        if not k.startswith('__'):
+    #            self.shell.push({k:v})
 
     def run_command(self, args, silent_except=False):
         try:
@@ -85,38 +77,20 @@ class BlockdiagMagics(Magics):
                       file=sys.stderr)
         except OSError as e:
             if not silent_except:
-            	print ('Exception %s' % str(e), file=sys.stderr)
+                print ('Exception %s' % str(e), file=sys.stderr)
         return False
-
-    def inkscape_available(self):
-        global _inkscape_available
-        if _inkscape_available is None:
-            _inkscape_available = self.run_command(['inkscape', '--export-png'], silent_except=True)
-        return _inkscape_available
-
-    def svg2png(self, filename):
-        self.run_command(['inkscape', filename + '.svg',
-                '--export-png=' + filename + '.png'])
 
     def diag(self, line, cell, command):
         """Create sequence diagram using supplied diag methods."""
         code = cell + u'\n'
-        # if inkscape is available create SVG for either case
-        if self.inkscape_available():
-            global _draw_mode
-            _draw_mode = 'SVG'
-
         try:
             tmpdir = tempfile.mkdtemp()
             fd, diag_name = tempfile.mkstemp(dir=tmpdir)
             f = os.fdopen(fd, "wb")
             f.write(code.encode('utf-8'))
             f.close()
-
-            format = _draw_mode.lower()
-            draw_name = diag_name + '.' + format
-
-            args = ['-T', format, '-o', draw_name, diag_name]
+            draw_name = diag_name + '.' + self._publish_mode
+            args = ['-T', self._publish_mode, '-o', draw_name, diag_name]
             # if os.path.exists(fontpath):
             #    sys.argv += ['-f', fontpath]
 
@@ -126,13 +100,9 @@ class BlockdiagMagics(Magics):
                 #~ args += ['--ignore-pil']
 
             command.main(args=args)
-            
-            if _draw_mode == 'SVG' and _publish_mode == 'PNG':
-                # render SVG with inkscape
-                self.svg2png(diag_name)
 
-            file_name = diag_name + '.' + _publish_mode.lower()
-            with io.open(file_name, 'rb') as f:
+            #file_name = diag_name + '.' + self._publish_mode
+            with io.open(draw_name, 'rb') as f:
                 data = f.read()
                 f.close()
 
@@ -141,7 +111,7 @@ class BlockdiagMagics(Magics):
                 os.unlink(tmpdir + "/" + file)
             os.rmdir(tmpdir)
 
-        if _publish_mode == 'SVG':
+        if self._publish_mode == 'svg':
             display(SVG(data))
             #publish_display_data(
                 #~ u'IPython.core.displaypub.publish_svg',
@@ -161,7 +131,7 @@ class BlockdiagMagics(Magics):
     @cell_magic
     def blockdiag(self, line, cell):
         import blockdiag.command
-        self.diag(line, cell, blockdiag.command)
+        self.diag(line, f'blockdiag {{ {cell} }}', blockdiag.command)
 
     @cell_magic
     def nwdiag(self, line, cell):
@@ -174,36 +144,29 @@ class BlockdiagMagics(Magics):
         self.diag(line, cell, seqdiag.command)
 
     @cell_magic
-    def packetdiag (self, line, cell):
+    def packetdiag(self, line, cell):
         """Depends on nwdiag."""
         import packetdiag.command
         self.diag(line, cell, packetdiag.command)
 
     @cell_magic
-    def rackdiag (self, line, cell):
+    def rackdiag(self, line, cell):
         """Depends on nwdiag."""
         import rackdiag.command
         self.diag(line, cell, rackdiag.command)
         
     @line_cell_magic
     def setdiagsvg(self, line, cell=None):
-        global _draw_mode, _publish_mode
-        _draw_mode = _publish_mode = 'SVG'
+        self._publish_mode = 'SVG'.lower()
 
     @line_cell_magic
     def setdiagpng(self, line, cell=None):
-        global _draw_mode, _publish_mode
-        _draw_mode = _publish_mode = 'PNG'
+        self._publish_mode = 'PNG'.lower()
 
-
-_loaded = False
 
 def load_ipython_extension(ip):
     """Load the extension in IPython."""
-    global _loaded
-    if not _loaded:
-        ip.register_magics(BlockdiagMagics)
-        _loaded = True
+    ipython.register_magics(BlockdiagMagics)
     
 ip = get_ipython()
 ip.register_magics(BlockdiagMagics)
