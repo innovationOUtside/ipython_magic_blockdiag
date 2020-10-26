4c11891a0812340040ce94f0563b3cc7831569eb
Tony Hirst <tony.hirst@gmail.com>
2020-10-26 14:41:08 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 
 setup(name='blockdiagMagic',
       packages=['blockdiag_magic'],
-      install_requires=['blockdiag']
+      install_requires=['blockdiag', 'seqdiag', 'nwdiag', 'actdiag']
 #also support a ipython_magic_blockdiag[svg] opton that installs Wand as well
      )
