29c19fe446d2d7daad27f301688d4070d963e94f
Tony Hirst <tony.hirst@gmail.com>
2020-10-26 16:00:24 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,15 @@
 from setuptools import setup
 
 setup(name='blockdiagMagic',
-      packages=['blockdiag_magic'],
-      install_requires=['blockdiag', 'seqdiag', 'nwdiag', 'actdiag']
-#also support a ipython_magic_blockdiag[svg] opton that installs Wand as well
+      author="Tony Hirst",
+     author_email="tony.hirst@gmail.com",
+     description="IPython magics for rendering blockdiag family diagrams.",
+     long_description='''
+    A collection of IPython cell magics for creating blockdiag, nwdiag and actdiag diagrams inline in Jupyter notebooks.
+    ''',
+     long_description_content_type="text/markdown",
+     packages=['blockdiag_magic'],
+     install_requires=['blockdiag', 'seqdiag', 'nwdiag', 'actdiag'],
+     version='0.0.2'
+      
      )
